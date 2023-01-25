4c461d13933bc3005b259f13040d58d7f01684b4
plfmoura <plfmoura96@gmail.com>
2023-01-25 19:49:58 -0300

    add(components)new example components, routes and other

---
+++ src/components/ExemploComponent/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ExemploComponent() {
+  return (
+    <div>
+      <h1>Primeiro Exemplo</h1>
+    </div>
+  )
+}
---
+++ src/components/SegundoExemplo/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function SegundoExemplo() {
+  return (
+    <div>
+      <input type="button" value="Segundo Exemplo" />
+    </div>
+  )
+}
