f78349431bc7f787b56efb1cbe1ccd24330c75d0
plfmoura <plfmoura96@gmail.com>
2023-01-28 23:36:39 -0300

    add(home)card movies and axios request

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,18 +1,37 @@
 import { Box } from '@mui/material'
-import React, { useState } from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 import HomeCard from '../components/HomeCard'
 
-const [ movies, setMovies ] = useState([])
-
 export default function Home() {
-  return (
-    <div>
-      <Box display={"flex"}>
-        {
 
-        }
-        <HomeCard />
-      </Box>
-    </div>
+  const [ movies, setMovies ] = useState([])
+
+  const getMovies = () => {
+    let url = `https://json-server-md3.onrender.com/filmes`
+    axios.get(url).then((response) => setMovies(response.data))
+  }
+
+  useEffect(() => {
+    getMovies()
+  }, [])
+
+  return (
+    <>
+      <div className="popularArea">
+        
+      </div>
+      <div style={{marginTop: "10%", backgroundColor: "#f1f1f1"}}>
+        <h1>Filmes em cartaz</h1>
+        <p>Trailers e horários dos filmes em cartaz nos cinemas</p>
+        <Box display={"flex"} flexWrap={"wrap"} margin={'2rem'} alignItems={"center"} justifyContent={"center"}>
+          {
+            movies.map(( item ) => 
+              <HomeCard title={item.nome} image={item.img[0]} />
+            )
+          }
+        </Box>
+      </div>
+    </>
   )
 }
