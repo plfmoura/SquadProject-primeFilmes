aef7d719aec15a33e54dba539ce090d317f58796
m4sterUniverse <mister-beta@hotmail.com>
2023-01-29 19:45:52 -0300

    alter(filmes) arrumando catálogo de filmes

--- src/components/HomeCard/index.jsx
+++ src/components/HomeCard/index.jsx
@@ -1,9 +1,7 @@
 import * as React from 'react';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import style from './card.module.css'
 
