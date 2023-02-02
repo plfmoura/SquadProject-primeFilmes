5a8ffabed48161cf60841eb1732168b54c5a97d9
plfmoura <plfmoura96@gmail.com>
2023-02-02 16:09:35 -0300

    fix(home)carousel img adress

--- src/components/HomeCarrousel/index.jsx
+++ src/components/HomeCarrousel/index.jsx
@@ -5,25 +5,25 @@ function HomeCarrousel() {
 
   const dataCarousel = [
     {
-      image: "public/imagens/HomeCarousel/fast10.jpg", 
+      image: "/imagens/HomeCarousel/fast10.jpg", 
       title: "Velozes e Furiosos 10", 
       sub: `"O novo filme da franquia Velozes e Furiosos ganhou um pôster destacando Dominic Toretto (Vin Diesel). O filme estreia em maio e ganhará seu primeiro trailer no dia 10 de fevereiro."`,
       align: 'center'
     },
     {
-      image: "public/imagens/HomeCarousel/guardians.jpg", 
+      image: "/imagens/HomeCarousel/guardians.jpg", 
       title: "Guardiões da Galáxia", 
       sub: `"Ainda não se sabem muitos detalhes da trama, mas a conclusão da trilogia acompanha o grupo de heróis desajustados precisa salvar o universo novamente enquanto Peter Quill tenta se ajustar à ausência de Gamora. O grupo também deve enfrentar Adam Warlock, revelado em uma cena pós-crédito do segundo longa. "`,
       align: "center"
     },
     {
-      image: "public/imagens/HomeCarousel/john wick 4.jpg", 
+      image: "/imagens/HomeCarousel/john wick 4.jpg", 
       title: "John Wick, Capitulo 4", 
       sub: `"Sem a esperança de um futuro promissor, o que acontecerá e qual será o destino do protagonista são incógnitas que além de atiçar a curiosidade do público, faz com que, inclusive, muitos fãs percam o sono e só pensem nesse assunto."`,
       align: "center"
     },
     {
-      image: "public/imagens/HomeCarousel/aquaman.jpg",
+      image: "/imagens/HomeCarousel/aquaman.jpg",
       title: "Aquaman, o Reino perdido", 
       sub: `"Embora não se saiba muito sobre a trama do novo filme, a produção deve contar com o retorno de Patrick Wilson (Invocação do Mal), como Mestre dos Oceanos, Amber Heard (Liga da Justiça), como Mera, além de Yahya Abdul-Matteen II (Watchmen), como o vingativo vilão o Arraia-Negra."`,
       align: "top"
