96c95f9c77657c9d86549f2626d4673a84e188d4
Guilherme Felipe Dos Santos <FELLIPEGUILHERME37@GMAIL.COM>
2023-02-02 15:38:40 -0300

    Atualização de Estilo, alterações Equipe.jsx
    Pagina de estilo criada utilizando module.css;
    
    importei a folha de estilo no Equipe.jsx
    foi utilizada como componente entre chaves nas classNames  junto com as declarações.
    
    declaraçoes das classNames foram alteradas para o padrão do camonCase

--- src/routes/Equipe/Equipe.jsx
+++ src/routes/Equipe/Equipe.jsx
@@ -3,124 +3,124 @@ import Natan from "/imagens/Natan.png";
 import Guilherme from "/imagens/Guilherme.png";
 import Francisco from "/imagens/Francisco.png";
 import Lucas from "/imagens/Lucas.png.jpeg";
-import "./equipe.css";
+import styles from'./equipe.module.css';
 import { FaInstagramSquare, FaGithub, FaLinkedin } from "react-icons/fa";
 
 function Equipe() {
   return (
     <>
       <center>
-        <h1 className="titulo-principal">Nossa Equipe</h1>
+        <h1 className={styles.TituloPrincipal}>Nossa Equipe</h1>
       </center>
-      <div className="container">
-        <div className="card">
-          <div className="content">
-            <div className="foto">
+      <div className={styles.container}>
+        <div className={styles.card}>
+          <div className={styles.content}>
+            <div className={styles.foto}>
                 <img src={Pedro} alt="Perfil-do-Colaborador" />
             </div>
               <h2>
                 <span>Pedro </span>Moura
               </h2>
               <h5>Facilitador</h5>
-              <div className="redes-sociais">
+              <div className={styles.RedesSociais}>
                 <a href="https://github.com/plfmoura">
-                  <FaGithub className="social-icons"/>
+                  <FaGithub className={styles.SocialIcons}/>
                 </a>
                 <a href="https://www.instagram.com/wpeedro/">
-                  <FaInstagramSquare className="social-icons"/>
+                  <FaInstagramSquare className={styles.SocialIcons}/>
                 </a>
                 <a href="https://www.linkedin.com/in/pedro-moura-48a29819a/">
-                  <FaLinkedin className="social-icons"/>
+                  <FaLinkedin className={styles.SocialIcons}/>
                 </a>
               </div>
           </div>
         </div>
 
-        <div className="card">
-          <div className="content">
-            <div className="foto">
+        <div className={styles.card}>
+          <div className={styles.content}>
+            <div className={styles.foto}>
                 <img src={Natan} alt="Perfil-do-Colaborador" />
             </div>
               <h2>
                 <span>Natan</span> Brito
               </h2>
               <h5>G. Engajamento</h5>
-              <div className="redes-sociais">
+              <div className={styles.RedesSociais}>
                 <a href="https://github.com/m4sterUniverse">
-                  <FaGithub className="social-icons"/>
+                  <FaGithub className={styles.SocialIcons}/>
                 </a>
                 <a href="">
-                  <FaInstagramSquare className="social-icons"/>
+                  <FaInstagramSquare className={styles.SocialIcons}/>
                 </a>
                 <a href="https://www.linkedin.com/in/natan-silva-brito-junior-13720a199/">
-                  <FaLinkedin className="social-icons"/>
+                  <FaLinkedin className={styles.SocialIcons}/>
                 </a>
               </div>
           </div>
         </div>
 
-        <div className="card">
-          <div className="content">
-            <div className="foto">
+        <div className={styles.card}>
+          <div className={styles.content}>
+            <div className={styles.foto}>
                 <img src={Guilherme} alt="Perfil-do-Colaborador" />
             </div>
               <h2>
                 <span>Guilherme </span>Felipe
               </h2>
               <h5>G. Conteúdo</h5>
-              <div className="redes-sociais">
+              <div className={styles.RedesSociais}>
                 <a href="https://github.com/GUINITH">
-                  <FaGithub className="social-icons"/>
+                  <FaGithub className={styles.SocialIcons}/>
                 </a>
                 <a href="https://www.instagram.com/gml_fp55/">
-                  <FaInstagramSquare className="social-icons"/>
+                  <FaInstagramSquare className={styles.SocialIcons}/>
                 </a>
                 <a href="https://www.linkedin.com/in/guilherme-felipe-dos-santos-b74b4721a/">
-                  <FaLinkedin className="social-icons"/>
+                  <FaLinkedin className={styles.SocialIcons}/>
                 </a>
               </div>
           </div>
         </div>
 
-        <div className="card">
-          <div className="content">
-            <div className="foto">
+        <div className={styles.card}>
+          <div className={styles.content}>
+            <div className={styles.foto}>
                 <img src={Francisco} />
             </div>
               <h2>
                 <span>Fancisco</span> William
               </h2>
               <h5>Co-Facilitador</h5>
-              <div className="redes-sociais">
+              <div className={styles.RedesSociais}>
                 <a href="https://github.com/WillianXG">
-                  <FaGithub className="social-icons"/>
+                  <FaGithub className={styles.SocialIcons}/>
                 </a>
                 <a href="">
-                  <FaInstagramSquare className="social-icons"/>
+                  <FaInstagramSquare className={styles.SocialIcons}/>
                 </a>
                 <a href="https://www.linkedin.com/in/francisco-willian-dos-santos-silva-36072b250/">
-                  <FaLinkedin className="social-icons"/>
+                  <FaLinkedin className={styles.SocialIcons}/>
                 </a>
               </div>
           </div>
         </div>
 
-        <div className="card">
-          <div className="content">
-            <div className="foto">
+        <div className={styles.card}>
+          <div className={styles.content}>
+            <div className={styles.foto}>
                 <img src={Lucas} alt="Perfil-do-Colaborador" />
             </div>
               <h2>Lucas Lopes</h2>
               <h5>Colaborador</h5>
-              <div className="redes-sociais">
+              <div className={styles.RedesSociais}>
                 <a href="https://github.com/lacmlopes">
-                  <FaGithub className="social-icons"/>
+                  <FaGithub className={styles.SocialIcons}/>
                 </a>
                 <a href="">
-                  <FaInstagramSquare className="social-icons"/>
+                  <FaInstagramSquare className={styles.SocialIcons}/>
                 </a>
                 <a href="">
-                  <FaLinkedin className="social-icons"/>
+                  <FaLinkedin className={styles.SocialIcons}/>
                 </a>
               </div>
           </div>
