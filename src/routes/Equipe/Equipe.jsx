d9cc61e1e971f037dbce4c3094dbaa477ba91d65
plfmoura <plfmoura96@gmail.com>
2023-02-01 23:51:32 -0300

    alter(equipe)fix bugs and styles conflicts

--- src/routes/Equipe/Equipe.jsx
+++ src/routes/Equipe/Equipe.jsx
@@ -1,115 +1,133 @@
-import Pedro from '../../components/imagens/Pedro.png';
-import Natan from '../../components/imagens/Natan.png';
-import Guilherme from '../../components/imagens/Guilherme.png';
-import Francisco from '../../components/imagens/Francisco.png';
-import Lucas from '../../components/imagens/Lucas.png.jpeg'
-import './equipe.css'
-import { FaInstagramSquare, FaGithub, FaLinkedin  } from 'react-icons/fa';
+import Pedro from "/imagens/Pedro.png";
+import Natan from "/imagens/Natan.png";
+import Guilherme from "/imagens/Guilherme.png";
+import Francisco from "/imagens/Francisco.png";
+import Lucas from "/imagens/Lucas.png.jpeg";
+import "./equipe.css";
+import { FaInstagramSquare, FaGithub, FaLinkedin } from "react-icons/fa";
 
-function Equipe(){
-    return(
+function Equipe() {
+  return (
     <>
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <center><h1>Nossa Equipe</h1></center>
-<div className="Conteiner">
-<div className="Card">
-<div className='content'>
-        <div className='Foto'>
-        <center><img src={Pedro} alt="Perfil-do-Colaborador" /></center>
+      <center>
+        <h1 className="titulo-principal">Nossa Equipe</h1>
+      </center>
+      <div className="container">
+        <div className="card">
+          <div className="content">
+            <div className="foto">
+                <img src={Pedro} alt="Perfil-do-Colaborador" />
+            </div>
+              <h2>
+                <span>Pedro </span>Moura
+              </h2>
+              <h5>Facilitador</h5>
+              <div className="redes-sociais">
+                <a href="https://github.com/plfmoura">
+                  <FaGithub className="social-icons"/>
+                </a>
+                <a href="https://www.instagram.com/wpeedro/">
+                  <FaInstagramSquare className="social-icons"/>
+                </a>
+                <a href="https://www.linkedin.com/in/pedro-moura-48a29819a/">
+                  <FaLinkedin className="social-icons"/>
+                </a>
+              </div>
+          </div>
         </div>
-       <center><h2><span>Pedro</span>Moura</h2></center> 
-        <center><h5>Facilitador</h5></center>
-        <center>
-        <br />
-        <div className='Redes-sociais'>
-        <a href="https://github.com/plfmoura">< FaGithub/></a>
-       <a href="https://www.instagram.com/wpeedro/"><FaInstagramSquare/></a>
-      <a href="https://www.linkedin.com/in/pedro-moura-48a29819a/"><FaLinkedin/></a>
-        </div>
-       </center>
-    </div>
-</div>
 
-<div className='Card'>
-    <div className='content'>
-        <div className='Foto'>
-        <center><img src={Natan} alt="Perfil-do-Colaborador" /></center>
+        <div className="card">
+          <div className="content">
+            <div className="foto">
+                <img src={Natan} alt="Perfil-do-Colaborador" />
+            </div>
+              <h2>
+                <span>Natan</span> Brito
+              </h2>
+              <h5>G. Engajamento</h5>
+              <div className="redes-sociais">
+                <a href="https://github.com/m4sterUniverse">
+                  <FaGithub className="social-icons"/>
+                </a>
+                <a href="">
+                  <FaInstagramSquare className="social-icons"/>
+                </a>
+                <a href="https://www.linkedin.com/in/natan-silva-brito-junior-13720a199/">
+                  <FaLinkedin className="social-icons"/>
+                </a>
+              </div>
+          </div>
         </div>
-       <center><h2><span>Natan</span> Brito</h2></center> 
-        <center><h5>G. Engajamento</h5></center>
-        <center>
-        <br />
-        <div className='Redes-sociais'>
-        <a href="https://github.com/m4sterUniverse">< FaGithub/></a>
-       <a href=""><FaInstagramSquare/></a>
-      <a href="https://www.linkedin.com/in/natan-silva-brito-junior-13720a199/"><FaLinkedin/></a>
-        </div>
-       </center>
-    </div>
-</div>
 
-<div className='Card'>
-    <div className='content'>
-        <div className='Foto'>
-        <center><img src={Guilherme} alt="Perfil-do-Colaborador" /></center>
+        <div className="card">
+          <div className="content">
+            <div className="foto">
+                <img src={Guilherme} alt="Perfil-do-Colaborador" />
+            </div>
+              <h2>
+                <span>Guilherme </span>Felipe
+              </h2>
+              <h5>G. Conteúdo</h5>
+              <div className="redes-sociais">
+                <a href="https://github.com/GUINITH">
+                  <FaGithub className="social-icons"/>
+                </a>
+                <a href="https://www.instagram.com/gml_fp55/">
+                  <FaInstagramSquare className="social-icons"/>
+                </a>
+                <a href="https://www.linkedin.com/in/guilherme-felipe-dos-santos-b74b4721a/">
+                  <FaLinkedin className="social-icons"/>
+                </a>
+              </div>
+          </div>
         </div>
-       <center><h2><span>Guilherme </span>Felipe</h2></center> 
-        <center><h5>G. Conteúdo</h5></center>
-        <center>
-        <br />
-        <div className='Redes-sociais'>
-        <a href="https://github.com/GUINITH">< FaGithub/></a>
-       <a href="https://www.instagram.com/gml_fp55/"><FaInstagramSquare/></a>
-      <a href="https://www.linkedin.com/in/guilherme-felipe-dos-santos-b74b4721a/"><FaLinkedin/></a>
-        </div>
-       </center>
-    </div>
-</div>
 
-<div className='Card'>
-    <div className='content'>
-        <div className='Foto'>
-        <center><img src={Francisco} /></center>
+        <div className="card">
+          <div className="content">
+            <div className="foto">
+                <img src={Francisco} />
+            </div>
+              <h2>
+                <span>Fancisco</span> William
+              </h2>
+              <h5>Co-Facilitador</h5>
+              <div className="redes-sociais">
+                <a href="https://github.com/WillianXG">
+                  <FaGithub className="social-icons"/>
+                </a>
+                <a href="">
+                  <FaInstagramSquare className="social-icons"/>
+                </a>
+                <a href="https://www.linkedin.com/in/francisco-willian-dos-santos-silva-36072b250/">
+                  <FaLinkedin className="social-icons"/>
+                </a>
+              </div>
+          </div>
         </div>
-       <center><h2><span>Fancisco</span> William</h2></center> 
-        <center><h5>Co-Facilitador</h5></center>
-        <center>
-        <br />
-        <div className='Redes-sociais'>
-        <a href="https://github.com/WillianXG">< FaGithub/></a>
-       <a href=""><FaInstagramSquare/></a>
-      <a href="https://www.linkedin.com/in/francisco-willian-dos-santos-silva-36072b250/"><FaLinkedin/></a>
-        </div>
-       </center>
-    </div>
-</div>
 
-<div className='Card'>
-    <div className='content'>
-        <div className='Foto'>
-        <center><img src={Lucas} alt="Perfil-do-Colaborador" /></center>
+        <div className="card">
+          <div className="content">
+            <div className="foto">
+                <img src={Lucas} alt="Perfil-do-Colaborador" />
+            </div>
+              <h2>Lucas Lopes</h2>
+              <h5>Colaborador</h5>
+              <div className="redes-sociais">
+                <a href="https://github.com/lacmlopes">
+                  <FaGithub className="social-icons"/>
+                </a>
+                <a href="">
+                  <FaInstagramSquare className="social-icons"/>
+                </a>
+                <a href="">
+                  <FaLinkedin className="social-icons"/>
+                </a>
+              </div>
+          </div>
         </div>
-       <center><h2>Lucas Lopes</h2></center> 
-        <center><h5>Colaborador</h5></center>
-        <center>
-        <br />
-        <div className='Redes-sociais'>
-        <a href="https://github.com/lacmlopes">< FaGithub/></a>
-       <a href=""><FaInstagramSquare/></a>
-      <a href=""><FaLinkedin/></a>
-        </div>
-       </center>
-    </div>
-</div>
-
-</div>
-
+      </div>
     </>
-    )
+  );
 }
 
-export default Equipe
+export default Equipe;
